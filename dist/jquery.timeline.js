04f338cde446bfa2198c145b4c6ce28d20c73efa
Stefano Marra <stefano.marra1987@gmail.com>
2018-06-18 20:27:53 +0200

    Add initial plugin from project

---
+++ dist/jquery.timeline.js
@@ -0,0 +1,170 @@
+// the semi-colon before function invocation is a safety net against concatenated
+// scripts and/or other plugins which may not be closed properly.
+;(function ($, window, document, undefined) {
+
+	"use strict";
+
+	// undefined is used here as the undefined global variable in ECMAScript 3 is
+	// mutable (ie. it can be changed by someone else). undefined isn't really being
+	// passed in so we can ensure the value of it is truly undefined. In ES5, undefined
+	// can no longer be modified.
+
+	// window and document are passed through as local variables rather than global
+	// as this (slightly) quickens the resolution process and can be more efficiently
+	// minified (especially when both are regularly referenced in your plugin).
+
+	/**
+  * jQuery custom plugin implement the roadmap functionality
+  */
+
+	$.fn.timeline = function (events, opts) {
+		if (!events instanceof Array) {
+			events = [];
+		}
+		var settings = $.extend({
+			slide: 1,
+			events_per_slide: 6,
+			event_template: '<li class="timeline__events__event">' + '<div class="event">' + '<div class="event__date">####DATE###</div>' + '<div class="event__content">####CONTENT###</div>' + '</div>' + '</li>',
+			navigation: {
+				prev: 'prev',
+				next: 'next'
+			}
+		}, opts);
+
+		var buildEvent = function (event, key) {
+			var html = settings.event_template;
+			html = html.replace('####DATE###', event.date);
+			html = html.replace('####CONTENT###', event.content);
+
+			var left = 100 / (settings.events_per_slide - 1) * key;
+
+			return $(html).css('left', left + '%');
+		};
+
+		return this.each(function () {
+			var _this = this;
+			var $this = $(this);
+			var currentSlide = settings.slide - 1;
+
+			/**
+    * Store events and settings
+    */
+			$this.data({
+				events: events,
+				settings: settings,
+				currentSlide: currentSlide
+			}).addClass('timeline');
+
+			var clear = function () {
+				$this.removeClass('timeline--initialized');
+
+				$this.find('.timeline__events').remove();
+				$this.find('.timeline__navigation').remove();
+			};
+
+			var buildEvents = function () {
+				var currentSlide = $this.data('currentSlide');
+				var settings = $this.data('settings');
+				var events = $this.data('events');
+
+				$('<ol/>', { class: 'timeline__events' }).append(events.slice(currentSlide * settings.events_per_slide, (currentSlide + 1) * settings.events_per_slide).map(buildEvent)).appendTo(_this);
+			};
+
+			var buildNavigation = function () {
+				var currentSlide = $this.data('currentSlide');
+
+				var buildNav = function (nav) {
+					switch (nav) {
+						case 'prev':
+							if (currentSlide > 0) {
+								return $('<li><a href="#" class="' + nav + '">' + settings.navigation[nav] + '</a></li>');
+							}
+							break;
+
+						case 'next':
+							if ((currentSlide + 1) * settings.events_per_slide < events.length) {
+								return $('<li><a href="#" class="' + nav + '">' + settings.navigation[nav] + '</a></li>');
+							}
+							break;
+					}
+
+					return $('<li></li>');
+				};
+
+				$('<ul/>', { class: 'timeline__navigation' }).append(Object.keys(settings.navigation).map(buildNav)).appendTo(_this);
+			};
+
+			var build = function () {
+
+				clear();
+
+				/**
+     * Init events
+     */
+				buildEvents();
+
+				/**
+     * Init navigation
+     */
+				buildNavigation();
+
+				/**
+     * Initialize
+     */
+				setTimeout(function () {
+					$this.addClass('timeline--initialized');
+				}, 100);
+			};
+
+			/**
+    * Build timeline
+    */
+			build();
+
+			/**
+    * Event Listeners
+    */
+			$('body').on('click', '.timeline .timeline__navigation a', function (e) {
+				e.preventDefault();
+
+				/**
+     * Handle prev click
+     */
+				if ($(this).hasClass('prev')) {
+
+					var currentSlide = $this.data('currentSlide');
+					if (currentSlide < 1) {
+						return false;
+					}
+
+					$this.data({
+						events: events,
+						settings: settings,
+						currentSlide: currentSlide - 1
+					});
+
+					build();
+				}
+
+				/**
+     * Handle next click
+     */
+				else {
+
+						var currentSlide = $this.data('currentSlide');
+						if ((currentSlide + 1) * settings.events_per_slide >= events.length) {
+							return false;
+						}
+
+						$this.data({
+							events: events,
+							settings: settings,
+							currentSlide: currentSlide + 1
+						});
+
+						build();
+					}
+			});
+		});
+	};
+})(jQuery, window, document);
---
+++ gulpfile.js
@@ -0,0 +1,119 @@
+var gulp = require('gulp'),
+    jshint = require('gulp-jshint'),
+    autoprefixer = require('autoprefixer'),
+    babel = require('gulp-babel'),
+    cleancss = require('gulp-clean-css'),
+    rename = require('gulp-rename'),
+    sass = require('gulp-sass'),
+    uglify = require('gulp-uglify'),
+    karma = require('karma'),
+    postcss = require('gulp-postcss'),
+    flexibility = require('postcss-flexibility');
+
+
+var SRC = {
+    js: 'src/*.js',
+    css: 'src/*.scss'
+};
+
+/* Directories */
+var DIRS = {
+    src: './src',
+    dest: './dist'
+};
+
+/**
+ * Error reporting helper function.
+ * Code from https://github.com/brendanfalkowski
+ *
+ * @param err
+ */
+var errorReport = function(err) {
+    var lineNumber = (err.lineNumber) ? 'LINE ' + err.lineNumber + ' -- ' : '';
+
+    notify({
+        title: 'Task failed [' + err.plugin + ']',
+        message: lineNumber + 'See console.',
+        sound: 'Basso'
+    }).write(err);
+
+    gutil.beep();
+
+    // Report the error on the console
+    var report = '';
+    var chalk = gutil.colors.bgMagenta.white;
+
+    report += chalk('TASK:') + ' [' + err.plugin + ']\n';
+    report += chalk('ISSUE:') + ' ' + err.message + '\n';
+    if (err.lineNumber) { report += chalk('LINE:') + ' ' + err.lineNumber + '\n'; }
+    if (err.fileName)   { report += chalk('FILE:') + ' ' + err.fileName + '\n'; }
+    console.log(report);
+
+    // Prevent the 'watch' task from stopping
+    this.emit('end');
+};
+
+// Lint JS
+gulp.task('lint:js', function() {
+    return gulp.src(SRC_JS)
+            .pipe(jshint())
+            .pipe(jshint.reporter('default'))
+            .pipe(jshint.reporter('fail'));
+});
+
+// Build CSS
+gulp.task('build:css', function() {
+    return gulp.src(SRC.css)
+        .pipe(sass({
+            compress: true
+        }))
+        .pipe(postcss([autoprefixer({
+            browsers: [
+                "Android 2.3",
+                "Android >= 4",
+                "Chrome >= 20",
+                "Firefox >= 24",
+                "Explorer >= 8",
+                "iOS >= 6",
+                "Opera >= 12",
+                "Safari >= 6"
+            ]
+        }), flexibility()]))
+        .pipe(cleancss())
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(gulp.dest(DIRS.dest));
+});
+
+gulp.task('build:js', function() {
+    return gulp.src(SRC.js)
+            .pipe(babel())
+            .pipe(gulp.dest(DIRS.dest))
+            .pipe(uglify({
+                preserveComments:'license'
+            }))
+            .pipe(rename({
+                suffix: '.min'
+            }))
+            .pipe(gulp.dest(DIRS.dest));
+});
+
+// TEST
+gulp.task('test', function (done) {
+    new karma.Server({
+      configFile: __dirname + '/karma.conf.js',
+      singleRun: true
+    }, function() {
+        done();
+    }).start();
+});
+
+// WATCH for file changes and rerun the task
+gulp.task('watch', function() {
+    gulp.watch(SRC.js, gulp.series('build:js'));
+    gulp.watch(SRC.css, gulp.series('build:css'));
+});
+
+// DEFAULT task
+gulp.task('default', gulp.series('build:js', 'build:css'));
